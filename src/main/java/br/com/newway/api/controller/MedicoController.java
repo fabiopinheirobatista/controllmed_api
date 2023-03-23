3fd28a0e06d3420d042251a93005060647d3a904
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2023-03-23 11:51:44 -0300

    Adicionando recursos de consulta por Paginação

--- src/main/java/br/com/newway/api/controller/MedicoController.java
+++ src/main/java/br/com/newway/api/controller/MedicoController.java
@@ -1,14 +1,18 @@
 package br.com.newway.api.controller;
 
 import br.com.newway.api.medico.DadosCadastroMedico;
+import br.com.newway.api.medico.DadosListagemMedico;
 import br.com.newway.api.medico.Medico;
 import br.com.newway.api.medico.MedicoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -19,9 +23,22 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
         System.out.println(dados);
     }
 
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 2, page = 0, sort = {"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
+    /*public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    }*/
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosCadastroMedico dados){
+        //
+    }
 }
